a049943e42763692e61f7b891237354063aaaad4
Jeff Johnson <jrjohnson@google.com>
2016-09-30 13:48:13 -0700

    Fix import that relied on deleted code

--- src/stackdriver-nozzle/dev/gcp_util.go
+++ src/stackdriver-nozzle/dev/gcp_util.go
@@ -2,7 +2,7 @@ package dev
 
 import (
 	"cloud.google.com/go/monitoring/apiv3"
-	"github.com/evandbrown/gcp-tools-release/src/firehose-to-fluentd/Godeps/_workspace/src/golang.org/x/net/context"
+	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
 )
