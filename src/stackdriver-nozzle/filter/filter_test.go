9ebfdb87e8459d85ac10d21786e2cc3fc17eeb6c
Jeff Johnson <jrjohnson@google.com>
2016-10-03 09:36:26 -0700

    [Red] Call HandleEvent on the filter
    - Missed this perviously so filter code was not getting exercised
    - Moved out SendALlEvents to a helper method
    - Added test cases for NYI code
    - Fail test MockFirehoseHandler gets a call when the HandleFn isn't set

--- src/stackdriver-nozzle/filter/filter_test.go
+++ src/stackdriver-nozzle/filter/filter_test.go
@@ -3,6 +3,7 @@ package filter_test
 import (
 	"github.com/cloudfoundry/sonde-go/events"
 	"github.com/evandbrown/gcp-tools-release/src/stackdriver-nozzle/filter"
+	"github.com/evandbrown/gcp-tools-release/src/stackdriver-nozzle/firehose"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,9 +13,11 @@ var _ = Describe("Filter", func() {
 	var (
 		mockFirehoseHandler MockFirehoseHandler
 	)
+
 	BeforeEach(func() {
 		mockFirehoseHandler = MockFirehoseHandler{}
 	})
+
 	It("can accept an empty filter and blocks all events", func() {
 		emptyFilter := []string{}
 		f, err := filter.New(mockFirehoseHandler, emptyFilter)
@@ -26,13 +29,37 @@ var _ = Describe("Filter", func() {
 			return nil
 		}
 
-		for _, val := range events.Envelope_EventType_value {
-			event := events.Envelope{}
-			eventType := events.Envelope_EventType(val)
-			event.EventType = &eventType
+		SendAllEvents(f)
+	})
+
+	It("can accept a single event to filter", func() {
+		singleFilter := []string{"Error"}
+		f, err := filter.New(mockFirehoseHandler, singleFilter)
+		Expect(err).To(BeNil())
+		Expect(f).NotTo(BeNil())
+
+		mockFirehoseHandler.HandleEventFn = func(envelope *events.Envelope) error {
+			Expect(envelope.GetEventType()).To(Equal(events.Envelope_Error))
+			return nil
 		}
+
+		SendAllEvents(f)
+	})
+
+	It("rejects invalid events", func() {
+		Fail("NYI")
 	})
 })
+
+func SendAllEvents(filter firehose.FirehoseHandler) {
+	for _, val := range events.Envelope_EventType_value {
+		eventType := events.Envelope_EventType(val)
+		event := events.Envelope{}
+		event.EventType = &eventType
+
+		filter.HandleEvent(&event)
+	}
+}
 
 type MockFirehoseHandler struct {
 	HandleEventFn func(envelope *events.Envelope) error
@@ -41,6 +68,8 @@ type MockFirehoseHandler struct {
 func (mfh MockFirehoseHandler) HandleEvent(envelope *events.Envelope) error {
 	if mfh.HandleEventFn != nil {
 		return mfh.HandleEventFn(envelope)
+	} else {
+		Fail("Unexpected call to HandleEvent")
 	}
 	return nil
 }
