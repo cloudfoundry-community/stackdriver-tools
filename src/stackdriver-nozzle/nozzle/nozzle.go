f7761de1dcb4a4771107f8d5f967f2b69c49c5c6
nrxus <andresnrx@gmail.com>
2016-10-04 14:37:17 -0700

    Post container metrics (#14)
    * refactored nozzle tests
    
    https://www.pivotaltracker.com/story/show/131454435
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    Signed-off-by: Alpha Chen <achen@pivotal.io>
    
    * post container metrics
    
    https://www.pivotaltracker.com/story/show/131454435
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    
    * try sending all container metrics before failing out
    
    Don't return early if one container metric fails to send.
    
    https://www.pivotaltracker.com/story/show/131454435
    
    Signed-off-by: Alpha Chen <achen@pivotal.io>
    
    * make each call to post container metric async
    
    https://www.pivotaltracker.com/story/show/131454435
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    
    * Fix mock being not-thread-safe
    
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/stackdriver-nozzle/nozzle/nozzle.go
+++ src/stackdriver-nozzle/nozzle/nozzle.go
@@ -1,9 +1,24 @@
 package nozzle
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/cloudfoundry/sonde-go/events"
 	"github.com/evandbrown/gcp-tools-release/src/stackdriver-nozzle/stackdriver"
 )
+
+type PostContainerMetricError struct {
+	Errors []error
+}
+
+func (e *PostContainerMetricError) Error() string {
+	errors := []string{}
+	for _, err := range e.Errors {
+		errors = append(errors, err.Error())
+	}
+	return strings.Join(errors, "\n")
+}
 
 type Nozzle struct {
 	StackdriverClient stackdriver.Client
@@ -11,17 +26,63 @@ type Nozzle struct {
 
 func (n *Nozzle) HandleEvent(eventsEnvelope *events.Envelope) error {
 	envelope := Envelope{eventsEnvelope}
+	labels := envelope.Labels()
 
 	switch envelope.GetEventType() {
+	case events.Envelope_ContainerMetric:
+		return n.postContainerMetrics(envelope)
 	case events.Envelope_ValueMetric:
 		valueMetric := envelope.GetValueMetric()
 		name := valueMetric.GetName()
 		value := valueMetric.GetValue()
 
-		err := n.StackdriverClient.PostMetric(name, value, envelope.Labels())
+		err := n.StackdriverClient.PostMetric(name, value, labels)
 		return err
 	default:
-		n.StackdriverClient.PostLog(envelope, envelope.Labels())
+		n.StackdriverClient.PostLog(envelope, labels)
 		return nil
 	}
+}
+
+func (n *Nozzle) postContainerMetrics(envelope Envelope) *PostContainerMetricError {
+	containerMetric := envelope.GetContainerMetric()
+
+	labels := envelope.Labels()
+	labels["applicationId"] = containerMetric.GetApplicationId()
+
+	errorsCh := make(chan error)
+
+	n.postContainerMetric(errorsCh, "diskBytesQuota", float64(containerMetric.GetDiskBytesQuota()), labels)
+	n.postContainerMetric(errorsCh, "instanceIndex", float64(containerMetric.GetInstanceIndex()), labels)
+	n.postContainerMetric(errorsCh, "cpuPercentage", float64(containerMetric.GetCpuPercentage()), labels)
+	n.postContainerMetric(errorsCh, "diskBytes", float64(containerMetric.GetDiskBytes()), labels)
+	n.postContainerMetric(errorsCh, "memoryBytes", float64(containerMetric.GetMemoryBytes()), labels)
+	n.postContainerMetric(errorsCh, "memoryBytesQuota", float64(containerMetric.GetMemoryBytesQuota()), labels)
+
+	errors := []error{}
+	for i := 0; i < 6; i++ {
+		err := <-errorsCh
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	if len(errors) == 0 {
+		return nil
+	} else {
+		return &PostContainerMetricError{
+			Errors: errors,
+		}
+	}
+}
+
+func (n *Nozzle) postContainerMetric(errorsCh chan error, name string, value float64, labels map[string]string) {
+	go func() {
+		err := n.StackdriverClient.PostMetric(name, value, labels)
+		if err != nil {
+			errorsCh <- fmt.Errorf("%v: %v", name, err.Error())
+		} else {
+			errorsCh <- nil
+		}
+	}()
 }
