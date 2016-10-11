7985311f68f38442962a7bc009a6d2abfbd816f7
Jeff Johnson <jrjohnson@google.com>
2016-10-11 11:41:27 -0700

    Resolve enum fields to strings for stackdriver payload
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/stackdriver-nozzle/nozzle/log_sink.go
+++ src/stackdriver-nozzle/nozzle/log_sink.go
@@ -3,6 +3,7 @@ package nozzle
 import (
 	"github.com/cloudfoundry-community/gcp-tools-release/src/stackdriver-nozzle/stackdriver"
 	"github.com/cloudfoundry/sonde-go/events"
+	"encoding/json"
 )
 
 func NewLogSink(labelMaker LabelMaker, logAdapter stackdriver.LogAdapter) Sink {
@@ -16,10 +17,49 @@ type logSink struct {
 
 func (lh *logSink) Receive(envelope *events.Envelope) error {
 	log := &stackdriver.Log{
-		Payload: envelope,
+		Payload: lh.buildPayload(envelope),
 		Labels:  lh.labelMaker.Build(envelope),
 	}
 
 	lh.logAdapter.PostLog(log)
 	return nil
+}
+
+func structToMap(obj interface{}) map[string]interface{} {
+	payload_json, _ := json.Marshal(obj)
+	var unmarshaled_map map[string]interface{}
+	json.Unmarshal(payload_json, &unmarshaled_map)
+
+	return unmarshaled_map
+}
+
+func (ls *logSink) buildPayload(envelope *events.Envelope) interface{} {
+	envelopeMap := structToMap(envelope)
+	envelopeMap["eventType"] = envelope.GetEventType().String()
+
+	// The json marshaling causes a loss in precision
+	if envelope.GetTimestamp() != 0 {
+		envelopeMap["timestamp"] = envelope.GetTimestamp()
+	}
+
+	switch envelope.GetEventType() {
+	case events.Envelope_LogMessage:
+		logMessage := envelope.GetLogMessage()
+		logMessageMap := structToMap(logMessage)
+		if logMessageMap != nil {
+			// TODO: should this be messageType?
+			logMessageMap["message_type"] = logMessage.GetMessageType().String()
+			envelopeMap["logMessage"] = logMessageMap
+		}
+	case events.Envelope_HttpStartStop:
+		httpStartStop := envelope.GetHttpStartStop()
+		httpStartStopMap := structToMap(httpStartStop)
+		if httpStartStopMap != nil {
+			httpStartStopMap["method"] = httpStartStop.GetMethod().String()
+			httpStartStopMap["peerType"] = httpStartStop.GetPeerType().String()
+			envelopeMap["httpStartStop"] = httpStartStopMap
+		}
+	}
+
+	return envelopeMap
 }
--- src/stackdriver-nozzle/nozzle/log_sink_test.go
+++ src/stackdriver-nozzle/nozzle/log_sink_test.go
@@ -1,6 +1,8 @@
 package nozzle_test
 
 import (
+	"time"
+
 	"github.com/cloudfoundry-community/gcp-tools-release/src/stackdriver-nozzle/mocks"
 	"github.com/cloudfoundry-community/gcp-tools-release/src/stackdriver-nozzle/nozzle"
 	"github.com/cloudfoundry/sonde-go/events"
@@ -24,15 +26,105 @@ var _ = Describe("LogSink", func() {
 		subject = nozzle.NewLogSink(labelMaker, logAdapter)
 	})
 
-	It("handles logs", func() {
+	It("passes fields through to the adapter", func() {
+		origin := "cool-origin"
 		eventType := events.Envelope_HttpStartStop
-		envelope := &events.Envelope{EventType: &eventType}
+		timestamp := int64(time.Now().UnixNano())
+		deployment := "neat-deployment"
+		job := "some-job"
+		index := "an-index"
+		ip := "192.168.1.1"
+		tags := map[string]string{
+			"foo": "bar",
+		}
+
+		method := events.Method_GET
+		peerType := events.PeerType_Client
+
+		event := events.HttpStartStop{
+			Method:   &method,
+			PeerType: &peerType,
+		}
+
+		envelope := &events.Envelope{
+			Origin:        &origin,
+			EventType:     &eventType,
+			Timestamp:     &timestamp,
+			Deployment:    &deployment,
+			Job:           &job,
+			Index:         &index,
+			Ip:            &ip,
+			Tags:          tags,
+			HttpStartStop: &event,
+		}
 
 		subject.Receive(envelope)
 
 		Expect(logAdapter.PostedLogs).To(HaveLen(1))
 		postedLog := logAdapter.PostedLogs[0]
-		Expect(postedLog.Payload).To(Equal(envelope))
 		Expect(postedLog.Labels).To(Equal(labels))
+
+		payload := (postedLog.Payload).(map[string]interface{})
+		Expect(payload).To(HaveKeyWithValue("eventType", "HttpStartStop"))
+		Expect(payload).To(HaveKeyWithValue("deployment", deployment))
+		Expect(payload).To(HaveKeyWithValue("job", job))
+		Expect(payload).To(HaveKeyWithValue("index", index))
+		Expect(payload).To(HaveKeyWithValue("ip", ip))
+		Expect(payload).To(HaveKeyWithValue("timestamp", timestamp))
+		Expect(payload).To(HaveKey("tags"))
+		Expect(payload["tags"].(map[string]interface{})).To(HaveKeyWithValue("foo", "bar"))
+	})
+
+	Describe("Payload translation", func() {
+		It("handles HttpStartStop", func() {
+			method := events.Method_GET
+			peerType := events.PeerType_Client
+			event := events.HttpStartStop{
+				Method:   &method,
+				PeerType: &peerType,
+			}
+
+			eventType := events.Envelope_HttpStartStop
+			envelope := &events.Envelope{
+				EventType:     &eventType,
+				HttpStartStop: &event,
+			}
+
+			subject.Receive(envelope)
+
+			postedLog := logAdapter.PostedLogs[0]
+			payload := (postedLog.Payload).(map[string]interface{})
+			Expect(payload).To(HaveKeyWithValue("eventType", "HttpStartStop"))
+			Expect(payload).To(HaveKey("httpStartStop"))
+			Expect(payload).To(HaveKeyWithValue("httpStartStop", map[string]interface{}{
+				"method":   "GET",
+				"peerType": "Client",
+			}))
+		})
+
+		It("has resolved labels and payloads equivalent for LogMessage", func() {
+			eventType := events.Envelope_LogMessage
+			messageType := events.LogMessage_OUT
+
+			event := events.LogMessage{
+				MessageType: &messageType,
+			}
+			envelope := &events.Envelope{
+				EventType:  &eventType,
+				LogMessage: &event,
+			}
+
+			subject.Receive(envelope)
+
+			postedLog := logAdapter.PostedLogs[0]
+			payload := (postedLog.Payload).(map[string]interface{})
+
+			Expect(payload).To(Equal(map[string]interface{}{
+				"eventType": eventType.String(),
+				"logMessage": map[string]interface{}{
+					"message_type": "OUT",
+				},
+			}))
+		})
 	})
 })
