6ae56d4d5b6eac7ad7a8bee3731fdb149509c356
nrxus <andresnrx@gmail.com>
2016-10-11 16:44:42 -0700

    Isolated integration deployment (#32)
    * Switch to isolated deployment (do not re-use cf)
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    
    * use different IP
    
    Signed-off-by: Matt Cholick <mcholick@pivotal.io>
    
    * tried to fix the IP yet again
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    
    * use latest version
    
    Signed-off-by: Matt Cholick <mcholick@pivotal.io>
    
    * update packing for go 1.7
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    
    * Uploaded the Go 1.7 blob
    
    Signed-off-by: Alpha Chen <achen@pivotal.io>
    
    * Missed passing down vip_ip
    Update credentials template
    
    Signed-off-by: Matt Cholick <mcholick@pivotal.io>

--- ci/credentials.yml.tpl
+++ ci/credentials.yml.tpl
@@ -15,6 +15,23 @@ bosh_user:             {{bosh_user}} # Bosh admin username
 bosh_password:         {{bosh_password} # Bosh password
 cf_deployment_name:    {{cf_deployment_name}} # Name of CF deployment to update
 
+# CF settings
+vip_ip:          {{replace_me}}
+common_password: {{replace_me}}
+
+# Google network settings
+google_region:      {{replace_me}}
+google_zone:        {{replace_me}}
+network:            {{replace_me}}
+public_subnetwork:  {{replace_me}}
+private_subnetwork: {{replace_me}}
+
+# Google service account settings
+project_id:              {{replace_me}}
+cf_service_account:      {{replace_me}}
+nozzle_user:             {{replace_me}}
+nozzle_password:         {{replace_me}}
+
 # GitHub
 github_deployment_key_bosh_google_cpi_release: | # GitHub deployment key for release artifacts
 github_pr_access_token: # An access token with repo:status access, used to test PRs
--- ci/tasks/deploy-candidate.sh
+++ ci/tasks/deploy-candidate.sh
@@ -5,10 +5,24 @@ set -e
 source gcp-tools-release/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
+# BOSH and CF config
 check_param bosh_director_address
 check_param bosh_user
 check_param bosh_password
-check_param cf_deployment_name
+
+# CF settings
+check_param vip_ip
+check_param nozzle_user
+check_param nozzle_password
+
+# Google network settings
+check_param google_zone
+check_param network
+check_param private_subnetwork
+
+# Google service account settings
+check_param project_id
+check_param cf_service_account
 
 echo "Using BOSH CLI version..."
 bosh version
@@ -16,11 +30,103 @@ bosh version
 echo "Targeting BOSH director..."
 bosh -n target ${bosh_director_address}
 bosh login ${bosh_user} ${bosh_password}
+director_uuid=$(bosh status --uuid)
 
 echo "Uploading nozzle release..."
 bosh upload release gcp-tools-release-artifacts/*.tgz
 
-echo "Downloading existing Cloud Foundry manifest"
-bosh download manifest ${cf_deployment_name} > cloudfoundry.yml
-bosh deployment cloudfoundry.yml
+nozzle_manifest_name=stackdriver-nozzle.yml
+cat > ${nozzle_manifest_name} <<EOF
+---
+
+name: stackdriver-nozzle-ci
+director_uuid: ${director_uuid}
+
+releases:
+- name: bosh-gcp-tools
+  version: latest
+
+jobs:
+- name: stackdriver-nozzle
+  instances: 1
+  networks:
+    - name: private
+  resource_pool: common
+  templates:
+    - name: stackdriver-nozzle
+      release: bosh-gcp-tools
+  properties:
+    firehose:
+      api_endpoint: https://api.${vip_ip}.xip.io
+      username: ${nozzle_user}
+      password: ${nozzle_password}
+      skip_ssl_validation: true
+    gcp:
+      project_id: ${project_id}
+
+compilation:
+  workers: 6
+  network: private
+  reuse_compilation_vms: true
+  cloud_properties:
+    zone: ${google_zone}
+    machine_type: n1-standard-8
+    root_disk_size_gb: 100
+    root_disk_type: pd-ssd
+    preemptible: true
+    service_account: ${cf_service_account}
+
+
+resource_pools:
+  - name: common
+    network: private
+    stemcell:
+      name: bosh-google-kvm-ubuntu-trusty-go_agent
+      version: latest
+    cloud_properties:
+      zone: ${google_zone}
+      machine_type: n1-standard-4
+      root_disk_size_gb: 20
+      root_disk_type: pd-standard
+      service_account: ${cf_service_account}
+  - name: nozzle
+    network: private
+    stemcell:
+      name: bosh-google-kvm-ubuntu-trusty-go_agent
+      version: latest
+    cloud_properties:
+      zone: ${google_zone}
+      machine_type: n1-standard-4
+      root_disk_size_gb: 20
+      root_disk_type: pd-standard
+      service_account: ${cf_service_account}
+
+networks:
+  - name: private
+    type: manual
+    subnets:
+    - range: 192.168.0.0/16
+      reserved:
+      - 192.168.0.0-192.168.1.15
+      gateway: 192.168.0.1
+      cloud_properties:
+        zone: ${google_zone}
+        network_name: ${network}
+        subnetwork_name: ${private_subnetwork}
+        ephemeral_external_ip: false
+        tags:
+          - stackdriver-nozzle-internal
+          - internal
+          - no-ip
+
+update:
+  canaries: 1
+  max_in_flight: 1
+  serial: false
+  canary_watch_time: 1000-60000
+  update_watch_time: 1000-60000
+
+EOF
+
+bosh deployment ${nozzle_manifest_name}
 bosh -n deploy
