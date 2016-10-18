ba31bf85b4b4869ad2fb1be492273b507cacd341
Andres Medina <amedina@pivotal.io>
2016-10-18 13:35:29 -0700

    updated user agent for metric client
    Signed-off-by: Jeff Johnson <jrjohnson@google.com>

--- src/stackdriver-nozzle/stackdriver/metric_client.go
+++ src/stackdriver-nozzle/stackdriver/metric_client.go
@@ -18,12 +18,10 @@ type MetricClient interface {
 
 func NewMetricClient() (MetricClient, error) {
 	ctx := context.Background()
-	sdMetricClient, err := monitoring.NewMetricClient(ctx, option.WithScopes("https://www.googleapis.com/auth/monitoring.write"))
+	sdMetricClient, err := monitoring.NewMetricClient(ctx, option.WithScopes("https://www.googleapis.com/auth/monitoring.write"), option.WithUserAgent(version.UserAgent))
 	if err != nil {
 		return nil, err
 	}
-
-	sdMetricClient.SetGoogleClientInfo(version.Name, version.Release)
 
 	return &metricClient{
 		sdMetricClient: sdMetricClient,
