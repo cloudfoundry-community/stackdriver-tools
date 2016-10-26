1b45eff8150f52e0a60177cfc356e6ea6144944a
Andres Medina <amedina@pivotal.io>
2016-10-26 16:50:56 -0700

    added script to get the rate of incoming firehose messages
    Signed-off-by: Matt Cholick <mcholick@pivotal.io>

---
+++ src/stackdriver-nozzle/scripts/firehose_rate.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/cloudfoundry-community/gcp-tools-release/src/stackdriver-nozzle/firehose"
+	"github.com/cloudfoundry-community/gcp-tools-release/src/stackdriver-nozzle/heartbeat"
+	"github.com/cloudfoundry-community/go-cfclient"
+	"github.com/cloudfoundry/lager"
+)
+
+func main() {
+	apiEndpoint := os.Getenv("FIREHOSE_ENDPOINT")
+	username := os.Getenv("FIREHOSE_USERNAME")
+	password := os.Getenv("FIREHOSE_PASSWORD")
+	_, skipSSLValidation := os.LookupEnv("FIREHOSE_SKIP_SSL")
+
+	cfConfig := &cfclient.Config{
+		ApiAddress:        apiEndpoint,
+		Username:          username,
+		Password:          password,
+		SkipSslValidation: skipSSLValidation}
+
+	cfClient := cfclient.NewClient(cfConfig)
+
+	client := firehose.NewClient(cfConfig, cfClient, "firehose-rate-script")
+
+	messages, _ := client.Connect()
+
+	trigger := time.Tick(1 * time.Second)
+	logger := lager.NewLogger("firehose-rate-script")
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+	heartbeater := heartbeat.NewHeartbeater(logger, trigger)
+	heartbeater.Start()
+	defer heartbeater.Stop()
+	for _ = range messages {
+		heartbeater.Increment("count")
+	}
+}
