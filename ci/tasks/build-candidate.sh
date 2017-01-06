eafb2235ff549bc5cad5da7a08e1ee70fee16173
Jeff Johnson <jrjohnson@google.com>
2017-01-06 14:31:13 -0600

    Correct naming

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -5,18 +5,18 @@ set -e
 source stackdriver-tools/ci/tasks/utils.sh
 source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
-cpi_release_name="stackdriver-tools"
+release_name="stackdriver-tools"
 semver=`cat version-semver/number`
 
 pushd stackdriver-tools
   echo "Using BOSH CLI version..."
   bosh version
 
-  echo "Creating CPI BOSH Release..."
-  bosh create release --name ${cpi_release_name} --version ${semver} --with-tarball
+  echo "Creating ${release_name} BOSH Release..."
+  bosh create release --name ${release_name} --version ${semver} --with-tarball
 popd
 
-image_path=stackdriver-tools/dev_releases/${cpi_release_name}/${cpi_release_name}-${semver}.tgz
+image_path=stackdriver-tools/dev_releases/${release_name}/${release_name}-${semver}.tgz
 echo -n $(sha1sum $image_path | awk '{print $1}') > $image_path.sha1
 
 mv ${image_path} candidate/
