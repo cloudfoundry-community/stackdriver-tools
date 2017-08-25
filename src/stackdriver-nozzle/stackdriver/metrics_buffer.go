80533af47d12837109ccb1396c58b5a96c6ba089
Jeff Johnson <jrjohnson@google.com>
2017-08-24 23:56:54 -0700

    remove dead code: MetricsBuffer

--- src/stackdriver-nozzle/stackdriver/metrics_buffer.go
+++ src/stackdriver-nozzle/stackdriver/metrics_buffer.go
@@ -16,74 +16,9 @@
 
 package stackdriver
 
-import (
-	"reflect"
-)
-
 type MetricsBuffer interface {
 	PostMetric(*Metric)
 	IsEmpty() bool
-}
-
-type metricsBuffer struct {
-	size    int
-	adapter MetricAdapter
-	errs    chan error
-	metrics []Metric
-}
-
-func NewMetricsBuffer(size int, adapter MetricAdapter) (MetricsBuffer, <-chan error) {
-	errs := make(chan error)
-	return &metricsBuffer{size, adapter, errs, []Metric{}}, errs
-}
-
-func (mb *metricsBuffer) PostMetric(metric *Metric) {
-	mb.addMetric(metric)
-
-	if len(mb.metrics) < mb.size {
-		return
-	}
-
-	mb.postMetrics(mb.metrics)
-	mb.metrics = []Metric{}
-}
-
-func (mb *metricsBuffer) IsEmpty() bool {
-	return len(mb.metrics) == 0
-}
-
-func (mb *metricsBuffer) addMetric(newMetric *Metric) {
-	var existingMetric *Metric
-
-	for _, metric := range mb.metrics {
-		if metric.Name == newMetric.Name &&
-			reflect.DeepEqual(metric.Labels, newMetric.Labels) {
-			existingMetric = &metric
-			break
-		}
-	}
-
-	if existingMetric == nil {
-		mb.metrics = append(mb.metrics, *newMetric)
-	} else {
-		/*
-			Stack driver API does not let us have multiple time series with the same name/label
-			in a single request. Furthermore, within each time series, we cannot have multiple points.
-			Due to this, if we encounter a metric with same name/labels, we will send the current buffer
-			and make a new buffer with the duplicate metric (╯°□°）╯︵ ┻━┻
-		*/
-		mb.postMetrics(mb.metrics)
-		mb.metrics = []Metric{*newMetric}
-	}
-}
-
-func (mb *metricsBuffer) postMetrics(metrics []Metric) {
-	go func() {
-		err := mb.adapter.PostMetrics(metrics)
-		if err != nil {
-			mb.errs <- err
-		}
-	}()
 }
 
 func metricsMapToSlice(m map[string]*Metric) []Metric {
