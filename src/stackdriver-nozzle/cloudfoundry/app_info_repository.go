cloudfoundry-community/stackdriver-tools

commit 0f4ecb18280bedc92e7437e7a2d9b0b01d9a2648

cloudfoundry: firehose returns err in new scenarios

src/stackdriver-nozzle/cloudfoundry/app_info_repository.go
@@ -30,15 +30,17 @@ type appInfoRepository struct {
 func (air *appInfoRepository) GetAppInfo(guid string) AppInfo {
 	appInfo, ok := air.cache[guid]
 	if !ok {
-		app := air.cfClient.AppByGuid(guid)
-		appInfo = AppInfo{
-			AppName:   app.Name,
-			SpaceGUID: app.SpaceData.Entity.Guid,
-			SpaceName: app.SpaceData.Entity.Name,
-			OrgGUID:   app.SpaceData.Entity.OrgData.Entity.Guid,
-			OrgName:   app.SpaceData.Entity.OrgData.Entity.Name,
+		app, err := air.cfClient.AppByGuid(guid)
+		if err != nil {
+			appInfo = AppInfo{
+				AppName:   app.Name,
+				SpaceGUID: app.SpaceData.Entity.Guid,
+				SpaceName: app.SpaceData.Entity.Name,
+				OrgGUID:   app.SpaceData.Entity.OrgData.Entity.Guid,
+				OrgName:   app.SpaceData.Entity.OrgData.Entity.Name,
+			}
+			air.cache[guid] = appInfo
 		}
-		air.cache[guid] = appInfo
 	}
 	return appInfo
 }

src/stackdriver-nozzle/cloudfoundry/firehose.go
@@ -68,8 +68,8 @@ func (ct *cfClientTokenRefresh) RefreshAuthToken() (token string, err error) {
 	//
 	// TODO: Track https://github.com/cloudfoundry-community/go-cfclient/issues/34 for
 	// updates on proper refresh token handling.
-	token = ct.cfClient.GetToken()
-	if token == "" {
+	token, err = ct.cfClient.GetToken()
+	if token == "" && err == nil {
 		err = fmt.Errorf("Fatal: error getting refresh token")
 	}
 	return

src/stackdriver-nozzle/main.go
@@ -142,7 +142,10 @@ func newApp() *app {
 		Username:          c.Username,
 		Password:          c.Password,
 		SkipSslValidation: c.SkipSSL}
-	cfClient := cfclient.NewClient(cfConfig)
+	cfClient, err := cfclient.NewClient(cfConfig)
+	if err != nil {
+		logger.Error("cfClient", err)
+	}
 
 	var appInfoRepository cloudfoundry.AppInfoRepository
 	if c.ResolveAppMetadata {

commit 230b059fd35185b6808f32c34db2578563aa7e59

cloudfoundry/app_info_repository: labels not resolved
Labels are not resolved for applications. This is due to a inverted
check on an err object.

regression introduced in: 0f4ecb18280be

src/stackdriver-nozzle/cloudfoundry/app_info_repository.go
@@ -31,7 +31,7 @@ func (air *appInfoRepository) GetAppInfo(guid string) AppInfo {
 	appInfo, ok := air.cache[guid]
 	if !ok {
 		app, err := air.cfClient.AppByGuid(guid)
-		if err != nil {
+		if err == nil {
 			appInfo = AppInfo{
 				AppName:   app.Name,
 				SpaceGUID: app.SpaceData.Entity.Guid,