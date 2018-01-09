cloudfoundry-community/gcp-tools-release

Load black/whitelists from a JSON file.
The complexity of blacklist / whitelist configuration is such
that it can't be shoehorned into a couple of env vars easily.
Instead, read this data from a JSON config file whose name is
placed into an env var.

src/stackdriver-nozzle/config/config_test.go
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package config_test
+package config
 
 import (
 	"os"
@@ -24,7 +24,6 @@ import (
 	. "github.com/onsi/gomega"
 
 	"cloud.google.com/go/compute/metadata"
-	"github.com/cloudfoundry-community/stackdriver-tools/src/stackdriver-nozzle/config"
 )
 
 var _ = Describe("Config", func() {
@@ -42,7 +41,7 @@ var _ = Describe("Config", func() {
 	})
 
 	It("returns valid config from environment", func() {
-		c, err := config.NewConfig()
+		c, err := NewConfig()
 
 		Expect(err).To(BeNil())
 		Expect(c.APIEndpoint).To(Equal("https://api.example.com"))
@@ -68,10 +67,37 @@ var _ = Describe("Config", func() {
 
 	})
 
+	It("parses valid event filter JSON", func() {
+		data := []byte(`
+{
+	"blacklist": [{
+		"sink": "metric",
+		"type": "name",
+		"regexp": "^gorouter\\."
+	}, {
+		"sink": "all",
+		"type": "job",
+		"regexp": "jerrbb"
+	}],
+	"whitelist": [{
+		"sink": "log",
+		"type": "name",
+		"regexp": "^MetronAgent\\."
+	}]
+}`)
+		c, err := NewConfig()
+		Expect(err).To(BeNil())
+		Expect(c.parseEventFilterJSON(data)).To(BeNil())
+		Expect(c.MetricBlacklist.Len()).To(Equal(2))
+		Expect(c.MetricWhitelist.Len()).To(Equal(0))
+		Expect(c.LogBlacklist.Len()).To(Equal(1))
+		Expect(c.LogWhitelist.Len()).To(Equal(1))
+	})
+
 	DescribeTable("required values aren't empty", func(envName string) {
 		os.Setenv(envName, "")
 
-		_, err := config.NewConfig()
+		_, err := NewConfig()
 
 		Expect(err).NotTo(BeNil())
 		Expect(err.Error()).To(ContainSubstring(envName))
@@ -86,17 +112,17 @@ var _ = Describe("Config", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_MONITORING", "")
 		})
 		It("is invalid with no events", func() {
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).To(HaveOccurred())
 		})
 		It("a single event for logging is valid", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_LOGGING", "LogMessage")
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).NotTo(HaveOccurred())
 		})
 		It("a single event for monitoring is valid", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_MONITORING", "ValueMetric")
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})

cloudfoundry-community/stackdriver-tools

src/stackdriver-nozzle/config/config_test.go
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
-package config_test
+package config
 
 import (
+	"bytes"
 	"os"
 
 	. "github.com/onsi/ginkgo"
@@ -24,8 +25,25 @@ import (
 	. "github.com/onsi/gomega"
 
 	"cloud.google.com/go/compute/metadata"
-	"github.com/cloudfoundry-community/stackdriver-tools/src/stackdriver-nozzle/config"
 )
+
+// Hoisted to top level because it's (even more) horrible inline.
+var validEventFilterJSON = bytes.NewBufferString(`{
+	"blacklist": [{
+		"sink": "monitoring",
+		"type": "name",
+		"regexp": "^gorouter\\."
+	}, {
+		"sink": "all",
+		"type": "job",
+		"regexp": "jerrbb"
+	}],
+	"whitelist": [{
+		"sink": "logging",
+		"type": "name",
+		"regexp": "^MetronAgent\\."
+	}]
+}`)
 
 var _ = Describe("Config", func() {
 
@@ -42,7 +60,7 @@ var _ = Describe("Config", func() {
 	})
 
 	It("returns valid config from environment", func() {
-		c, err := config.NewConfig()
+		c, err := NewConfig()
 
 		Expect(err).To(BeNil())
 		Expect(c.APIEndpoint).To(Equal("https://api.example.com"))
@@ -71,7 +89,7 @@ var _ = Describe("Config", func() {
 	DescribeTable("required values aren't empty", func(envName string) {
 		os.Setenv(envName, "")
 
-		_, err := config.NewConfig()
+		_, err := NewConfig()
 
 		Expect(err).NotTo(BeNil())
 		Expect(err.Error()).To(ContainSubstring(envName))
@@ -86,19 +104,52 @@ var _ = Describe("Config", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_MONITORING", "")
 		})
 		It("is invalid with no events", func() {
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).To(HaveOccurred())
 		})
 		It("a single event for logging is valid", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_LOGGING", "LogMessage")
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).NotTo(HaveOccurred())
 		})
 		It("a single event for monitoring is valid", func() {
 			os.Setenv("FIREHOSE_EVENTS_TO_STACKDRIVER_MONITORING", "ValueMetric")
-			_, err := config.NewConfig()
+			_, err := NewConfig()
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 
+	DescribeTable("parses empty-but-valid JSON files without errors", func(data string) {
+		c, err := NewConfig()
+		Expect(err).To(BeNil())
+		b := bytes.NewBufferString(data)
+		Expect(c.parseEventFilterJSON(b)).To(BeNil())
+		Expect(c.EventFilterJSON.Blacklist).To(HaveLen(0))
+		Expect(c.EventFilterJSON.Whitelist).To(HaveLen(0))
+	},
+		Entry("handles zero-length file", ""),
+		Entry("parses JSON with no lists", "{}"),
+		Entry("parses JSON with empty blacklist", `{"blacklist":[]}`),
+		Entry("parses JSON with empty whitelist", `{"whitelist":[]}`),
+		Entry("parses JSON with empty lists", `{"blacklist":[], "whitelist":[]}`),
+		// This is due to the unmarshalling behaviour of encoding/json.
+		Entry("ignores invalid lists", `{"yellowlist":[{"sink": "foo", "type": "name", "regexp": ".*"}]}`),
+	)
+
+	It("Errors on invalid JSON files", func() {
+		c, err := NewConfig()
+		Expect(err).To(BeNil())
+		b := bytes.NewBufferString(`{blacklist:[],}`)
+		Expect(c.parseEventFilterJSON(b)).NotTo(BeNil())
+		Expect(c.EventFilterJSON.Blacklist).To(HaveLen(0))
+		Expect(c.EventFilterJSON.Whitelist).To(HaveLen(0))
+	})
+
+	It("parses valid event filter JSON", func() {
+		c, err := NewConfig()
+		Expect(err).To(BeNil())
+		Expect(c.parseEventFilterJSON(validEventFilterJSON)).To(BeNil())
+		Expect(c.EventFilterJSON.Blacklist).To(HaveLen(2))
+		Expect(c.EventFilterJSON.Whitelist).To(HaveLen(1))
+	})
 })